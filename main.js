c30d71e109e988a140a71390d0c04a95b03f7644
Daniel Monge <danielmonge@MacBook-Air-de-Daniel.local>
2020-05-05 17:06:14 -0600

    images changes

--- main.js
+++ main.js
@@ -29,17 +29,17 @@ window.addEventListener('load', () => {
 	});
 
 	// Agregamos listener para las imagenes
-	const overlay = document.getElementById('overlay');
-	document.querySelectorAll('.grid .item img').forEach((elemento) => {
-		elemento.addEventListener('click', () => {
-			const ruta = elemento.getAttribute('src');
-			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+	// const overlay = document.getElementById('overlay');
+	// document.querySelectorAll('.grid .item img').forEach((elemento) => {
+	// 	elemento.addEventListener('click', () => {
+	// 		const ruta = elemento.getAttribute('src');
+	// 		const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
 
-			overlay.classList.add('activo');
-			document.querySelector('#overlay img').src = ruta;
-			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
-		});
-	});
+	// 		overlay.classList.add('activo');
+	// 		document.querySelector('#overlay img').src = ruta;
+	// 		document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+	// 	});
+	// });
 
 	// Eventlistener del boton de cerrar
 	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
